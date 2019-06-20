00fb88c2cb2156da71ac16cf3e1e37c49ffdc7ef
tenderjk <294400217@qq.com>
2019-06-20 22:09:05 +0800

    删除footer

--- src/layout/App.js
+++ src/layout/App.js
@@ -8,27 +8,9 @@ function App() {
         <Route path = '/user' component = { User } />
         <Redirect to = "/home"/>
       </Switch>
-      <footer className = "footer">
-        <ul>
-          <NavLink to = "/home">
-            <span>加的水电费是的是浮点数</span>
-            <p>首页11111111</p>
-          </NavLink>
-          <NavLink to = "/kind">
-            <span></span>
-            <p>分类</p>
-          </NavLink>
-          <NavLink to = "/cart">
-            <span></span>
-            <p>购物车</p>
-          </NavLink>
-          <NavLink to = "/user">
-            <span></span>
-            <p>我的</p>
-          </NavLink>
-        </ul>
-      </footer>
+
     </div>
+    // dasfw 
   );
 }
 
