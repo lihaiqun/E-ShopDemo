2a91de21c875aecad2e03bfa4529b858f68185f0
lihaiqun <1289181815@qq.com>
2019-06-20 21:49:20 +0800

    修改了ignore

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
-  
+
 })
 const store = createStore (reducer, applyMiddleware(thunk));
 export default store;
