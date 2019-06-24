7c5af0a750ec0584ff2297864d443cd37b364895
PoisonXu <909611179@qq.com>
2019-06-24 21:12:26 +0800

    单品初步完成

---
+++ src/components/find/Single/singleProlist.js
@@ -0,0 +1,61 @@
+import React from 'react';
+export default () => (
+  <ul className = "singleProlist">
+    <li>
+      <a href = "##">
+        <img src = "##" alt ="图片真的马上就来了" />
+        <p className = "produce">2018新款尖头浅口猫跟鞋</p>
+        <p className = "price"><span>￥</span>120.0</p>
+      </a>
+    </li>
+    <li>
+      <a href = "##">
+        <img src = "##" alt ="图片真的马上就来了" />
+        <p className = "produce">2018新款尖头浅口猫跟鞋</p>
+        <p className = "price"><span>￥</span>120.0</p>
+      </a>
+    </li>
+    <li>
+      <a href = "##">
+        <img src = "##" alt ="图片真的马上就来了" />
+        <p className = "produce">2018新款尖头浅口猫跟鞋</p>
+        <p className = "price"><span>￥</span>120.0</p>
+      </a>
+    </li>
+    <li>
+      <a href = "##">
+        <img src = "##" alt ="图片真的马上就来了" />
+        <p className = "produce">2018新款尖头浅口猫跟鞋</p>
+        <p className = "price"><span>￥</span>120.0</p>
+      </a>
+    </li>
+    <li>
+      <a href = "##">
+        <img src = "##" alt ="图片真的马上就来了" />
+        <p className = "produce">2018新款尖头浅口猫跟鞋</p>
+        <p className = "price"><span>￥</span>120.0</p>
+      </a>
+    </li>
+    <li>
+      <a href = "##">
+        <img src = "##" alt ="图片真的马上就来了" />
+        <p className = "produce">2018新款尖头浅口猫跟鞋</p>
+        <p className = "price"><span>￥</span>120.0</p>
+      </a>
+    </li>
+    <li>
+      <a href = "##">
+        <img src = "##" alt ="图片真的马上就来了" />
+        <p className = "produce">2018新款尖头浅口猫跟鞋</p>
+        <p className = "price"><span>￥</span>120.0</p>
+      </a>
+    </li>
+    <li>
+      <a href = "##">
+        <img src = "##" alt ="图片真的马上就来了" />
+        <p className = "produce">2018新款尖头浅口猫跟鞋</p>
+        <p className = "price"><span>￥</span>120.0</p>
+      </a>
+    </li>
+  </ul>
+)
--- src/views/find/findSingle/UI.js
+++ src/views/find/findSingle/UI.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '@/css/find/single.scss';
+import SingleProlist from '@/components/find/Single/singleProlist';
 import { SearchBar } from 'antd-mobile';
 class Com extends React.Component {
   componentDidMount () {
@@ -6,8 +8,21 @@ class Com extends React.Component {
   }
   render () {
     return (
-      <div className = "contianer">
-        <SearchBar placeholder="Search" maxLength={8} />
+      <div className = "container">
+        {/* <SearchBar placeholder="Search" maxLength={8} /> */}
+        <header className = "find_findSingle-header">
+          <span>&lt;返回</span>
+          <div className = "search">搜索</div>
+        </header>
+        <main className = "find_findSingle-main">
+          <ul className = "checkWay">
+            <li>默认</li>
+            <li>销量</li>
+            <li>价格</li>
+            <li>筛选&gt;</li>
+          </ul>
+          <SingleProlist />
+        </main>
       </div>
     )
   }
