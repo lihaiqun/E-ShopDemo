dfeded403502754375587740318d15d8d2d64039
tenderjk <294400217@qq.com>
2019-06-26 22:29:53 +0800

    custom complete

--- src/views/custom/UI.js
+++ src/views/custom/UI.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@/css/custom/custom.scss'
 import Header from '@/components/common/header'
+import { Link } from 'react-router-dom'
 class Custom extends React.Component {
   constructor(props) {
     super(props)
@@ -13,7 +14,24 @@ class Custom extends React.Component {
         <Header name="私人订制" history={this.props.history} />
         <img className="banner" />
         <div className="info-container">
-          
+          <h4>
+            搭配测试
+          </h4>
+          <p className="basic-info">1.基本信息</p>
+          <div className="select-sex">
+            <label><div><input name="sex" type="radio" /><span></span></div>女生</label>
+            <label><div><input name="sex" type="radio" /><span></span></div>男生</label>
+          </div>
+          <div className="age select">
+            年龄<input type="text" />岁
+          </div>
+          <div className="weight select">
+            体重<input type="text" />kg
+          </div>
+          <div className="height select">
+            身高<input type="text" />cm
+          </div>
+          <Link className="jump" to = '/homes/custom/body'>下一步</Link>
         </div>
       </main>
     )
