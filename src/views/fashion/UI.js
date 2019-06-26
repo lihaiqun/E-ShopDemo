730e4fc9b448603193e606f940605efd5f2a3469
lihaiqun <1289181815@qq.com>
2019-06-26 22:46:43 +0800

    user和fashion添加图片

--- src/views/fashion/UI.js
+++ src/views/fashion/UI.js
@@ -20,7 +20,7 @@ class Com extends React.Component {
       <div id = "fashion">
         <header className = "fashion_header">
           <div className = "search">
-            <i>图标</i><p>大家都在搜"连衣裙"</p>
+            <i><img src = {require("@/images/fashion/search.png")} alt = "" /></i><p>大家都在搜"连衣裙"</p>
           </div>
           </header>
           <div className = "fashion_list">
@@ -35,58 +35,96 @@ class Com extends React.Component {
               <ul className = "fashion_all">
                 <li>
                   <div className = "list_title">
-                    <img className = "title_img" src = "" alt = "图片马上来" />
+                  <img className = "title_img" src = {require("@/images/fashion/avater1.png")} alt = "" />
                     <div className = "title">
                       <p className = "title_name">萌喵酱</p>
                       <p className = "title_tip">网络红人 170cm/50kg</p>
                     </div>
                   </div>
-                  <img className = "show_img" src = "" alt = "图片马上来" />
+                  <div className = "show_img">
+                    <img src = {require("@/images/fashion/share1-1.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share1-2.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share1-3.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share1-4.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share1-5.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share1-7.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share1-7.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share1-8.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share1-4.png")} alt = "图片马上来" />
+                  </div>
                   <div className = "nickname">#假两件卫衣裙</div>
                   <div className = "share_way">
-                    <div className = "like"><i>图标</i><span>喜欢</span></div>
-                    <div className = "share_to"><i>图标</i><span>分享</span></div>
-                    <div className = "weibo"><i>图标</i></div>
+                    <div className = "like"><i><img src = {require("@/images/fashion/sharelink.png")} alt = "图片马上来" /></i><span>喜欢</span></div>
+                    <div className = "share_to"><i><img src = {require("@/images/fashion/sharelink.png")} alt = "图片马上来" /></i><span>分享</span></div>
+                    <div className = "weibo"><i>3个小时前</i></div>
                   </div>
                 </li>
               </ul>
               </div>
               <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: 'auto', backgroundColor: '#fff' }}>
               <ul className = "fashion_all">
-                <li>
+              <li>
                   <div className = "list_title">
-                    <img className = "title_img" src = "" alt = "图片马上来" />
+                  <img className = "title_img" src = {require("@/images/fashion/avater2.png")} alt = "" />
                     <div className = "title">
-                      <p className = "title_name">萌喵酱</p>
-                      <p className = "title_tip">网络红人 170cm/50kg</p>
+                      <p className = "title_name">蓝枫</p>
+                      <p className = "title_tip">当红明星 170cm/50kg</p>
                     </div>
                   </div>
-                  <img className = "show_img" src = "" alt = "图片马上来" />
+                  <div className = "show_img">
+                    <img src = {require("@/images/fashion/share2-1.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share2-2.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share2-3.png")} alt = "图片马上来" />
+                  </div>
                   <div className = "nickname">#假两件卫衣裙</div>
                   <div className = "share_way">
-                    <div className = "like"><i>图标</i><span>喜欢</span></div>
-                    <div className = "share_to"><i>图标</i><span>分享</span></div>
-                    <div className = "weibo"><i>图标</i></div>
+                    <div className = "like"><i><img src = {require("@/images/fashion/sharelink.png")} alt = "图片马上来" /></i><span>喜欢</span></div>
+                    <div className = "share_to"><i><img src = {require("@/images/fashion/sharelink.png")} alt = "图片马上来" /></i><span>分享</span></div>
+                    <div className = "weibo"><i>3个小时前</i></div>
+                  </div>
+                </li>
+                <li>
+                  <div className = "list_title">
+                  <img className = "title_img" src = {require("@/images/fashion/avater4.png")} alt = "" />
+                    <div className = "title">
+                      <p className = "title_name">萌喵酱</p>
+                      <p className = "title_tip">当红明星 170cm/50kg</p>
+                    </div>
+                  </div>
+                  <div className = "show_img">
+                    <img src = {require("@/images/fashion/share1-1.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share1-2.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share1-3.png")} alt = "图片马上来" />
+                  </div>
+                  <div className = "nickname">#假两件卫衣裙</div>
+                  <div className = "share_way">
+                    <div className = "like"><i><img src = {require("@/images/fashion/sharelink.png")} alt = "图片马上来" /></i><span>喜欢</span></div>
+                    <div className = "share_to"><i><img src = {require("@/images/fashion/sharelink.png")} alt = "图片马上来" /></i><span>分享</span></div>
+                    <div className = "weibo"><i>3个小时前</i></div>
                   </div>
                 </li>
               </ul>
               </div>
               <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: 'auto', backgroundColor: '#fff' }}>
               <ul className = "fashion_all">
-                <li>
+              <li>
                   <div className = "list_title">
-                    <img className = "title_img" src = "" alt = "图片马上来" />
+                  <img className = "title_img" src = {require("@/images/fashion/avater3.png")} alt = "" />
                     <div className = "title">
-                      <p className = "title_name">萌喵酱</p>
-                      <p className = "title_tip">网络红人 170cm/50kg</p>
+                      <p className = "title_name">爱时尚</p>
+                      <p className = "title_tip">素人 170cm/50kg</p>
                     </div>
                   </div>
-                  <img className = "show_img" src = "" alt = "图片马上来" />
+                  <div className = "show_img">
+                    <img src = {require("@/images/fashion/share3-1.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share3-2.png")} alt = "图片马上来" />
+                    <img src = {require("@/images/fashion/share3-3.png")} alt = "图片马上来" />
+                  </div>
                   <div className = "nickname">#假两件卫衣裙</div>
                   <div className = "share_way">
-                    <div className = "like"><i>图标</i><span>喜欢</span></div>
-                    <div className = "share_to"><i>图标</i><span>分享</span></div>
-                    <div className = "weibo"><i>图标</i></div>
+                    <div className = "like"><i><img src = {require("@/images/fashion/sharelink.png")} alt = "图片马上来" /></i><span>喜欢</span></div>
+                    <div className = "share_to"><i><img src = {require("@/images/fashion/sharelink.png")} alt = "图片马上来" /></i><span>分享</span></div>
+                    <div className = "weibo"><i>3个小时前</i></div>
                   </div>
                 </li>
               </ul>
--- src/views/user/UI.js
+++ src/views/user/UI.js
@@ -11,32 +11,35 @@ class Com extends React.Component {
         <header className = "header">
           <div className = "logSetting">
             <div className = "logRes">
-              <img src = '' alt = "" />
+              <img src = {require("@/images/user/avater.png")} alt = "" />
+              <span>桃夭</span>
             </div>
-            <div className = "setting">设置</div>
+            <div className = "setting">
+              <img src = {require("@/images/user/setting.png")} alt = "" />
+            </div>
           </div>
           <ul className = "likeAttention">
-            <li><i></i><p>我的喜欢</p></li>
-            <li><i></i><p>我的关注</p></li>
-            <li><i></i><p>签到</p></li>
+            <li><i><img src = {require("@/images/user/heart.png")} alt = "" /></i><p>我的喜欢</p></li>
+            <li><i><img src = {require("@/images/user/star.png")} alt = "" /></i><p>我的关注</p></li>
+            <li><i><img src = {require("@/images/user/right.png")} alt = "" /></i><p>签到</p></li>
           </ul>
         </header>
         <div className = "content">
           <div className = "myOrder">
-            <i></i><p>我的订单</p>
+            <p>我的订单</p>
+            <div className = "allOrder"><span>全部订单</span><i><img src = {require("@/images/user/row.png")} alt = "" /></i></div>
           </div>
           <ul className = "pay">
-            <li><i></i><p>待付款</p></li>
-            <li><i></i><p>待发货</p></li>
-            <li><i></i><p>待收货</p></li>
-            <li><i></i><p>待评价</p></li>
-            <li><i></i><p>售后</p></li>
+            <li><i><img src = {require("@/images/user/unpay.png")} alt = "" /></i><p>待付款</p></li>
+            <li><i><img src = {require("@/images/user/payed.png")} alt = "" /></i><p>待发货</p></li>
+            <li><i><img src = {require("@/images/user/unevaluate.png")} alt = "" /></i><p>待评价</p></li>
+            <li><i><img src = {require("@/images/user/refund.png")} alt = "" /></i><p>售后/退款</p></li>
           </ul>
           <ul className = "couponList">
-            <Link to = "/users/myCoupon"><i></i><span>我的优惠券</span><b></b></Link>
-            <Link to = "/users/myReward"><i></i><span>推荐奖励</span><b></b></Link>
-            <Link to = "/users/myFootprint"><i></i><span>我的足迹</span><b></b></Link>
-            <Link to = "/users/myService"><i></i><span>在线客服</span><b></b></Link>
+            <Link to = "/users/myCoupon"><i><img src = {require("@/images/user/coupont.png")} alt = "" /></i><span>我的优惠券</span><b><img src = {require("@/images/user/row.png")} alt = "" /></b></Link>
+            <Link to = "/users/myReward"><i><img src = {require("@/images/user/reward.png")} alt = "" /></i><span>推荐奖励</span><b><img src = {require("@/images/user/row.png")} alt = "" /></b></Link>
+            <Link to = "/users/myFootprint"><i><img src = {require("@/images/user/footprint.png")} alt = "" /></i><span>我的足迹</span><b><img src = {require("@/images/user/row.png")} alt = "" /></b></Link>
+            <Link to = "/users/myService"><i><img src = {require("@/images/user/service.png")} alt = "" /></i><span>在线客服</span><b><img src = {require("@/images/user/row.png")} alt = "" /></b></Link>
           </ul>
         </div>
       </div>
